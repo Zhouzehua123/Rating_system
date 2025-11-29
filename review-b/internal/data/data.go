89a40bedd188ef62eb92d683e0363d68e19fc0a6
547222232 <547222232@qq.com>
2025-11-30 01:05:31 +0800

    服务注册与发现

--- review-b/internal/data/data.go
+++ review-b/internal/data/data.go
@@ -2,18 +2,22 @@ package data
 
 import (
 	"context"
+
 	v1 "review-b/api/review/v1"
 	"review-b/internal/conf"
 
+	"github.com/go-kratos/kratos/contrib/registry/consul/v2"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/middleware/validate"
+	"github.com/go-kratos/kratos/v2/registry"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/google/wire"
+	"github.com/hashicorp/consul/api"
 )
 
 // ProviderSet is data providers.
-var ProviderSet = wire.NewSet(NewReviewServiceClient, NewData, NewBusinessRepo)
+var ProviderSet = wire.NewSet(NewDiscovery, NewReviewServiceClient, NewData, NewBusinessRepo)
 
 // Data .
 type Data struct {
@@ -33,12 +37,26 @@ func NewData(c *conf.Data, rc v1.ReviewClient, logger log.Logger) (*Data, func()
 	}, cleanup, nil
 }
 
+func NewDiscovery(conf *conf.Registry) registry.Discovery {
+	c := api.DefaultConfig()
+	c.Address = conf.Consul.Address
+	c.Scheme = conf.Consul.Scheme
+	client, err := api.NewClient(c)
+	if err != nil {
+		panic(err)
+	}
+	dis := consul.New(client)
+	return dis
+}
+
 // 创建一个连接 review-service服务的grpc client端
-func NewReviewServiceClient() v1.ReviewClient {
+func NewReviewServiceClient(d registry.Discovery) v1.ReviewClient {
 	//这里要实现一个grpc的client去连接review-service服务
 	conn, err := grpc.DialInsecure(
 		context.Background(),
-		grpc.WithEndpoint("127.0.0.1:9092"),
+		//grpc.WithEndpoint("127.0.0.1:9092"),
+		grpc.WithEndpoint("discovery:///review-service"),
+		grpc.WithDiscovery(d),
 		grpc.WithMiddleware(
 			recovery.Recovery(),
 			validate.Validator(),
